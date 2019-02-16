41305cd8848bfb39b3c6906e913de96172fe781b
KlavsLv <boisxlv16@gmail.com>
2019-02-16 13:55:13 +0200

    Kalkulators

---
+++ src/lv/homework1/Calculator.java
@@ -0,0 +1,22 @@
+package lv.homework1;
+
+public class Kalkulators {
+
+
+    public class Kalkulators{public static void main(String[]args)}}
+    int firstNumber = 10;
+    int secondNumber = 5;
+
+    int sumResult = firstNumber + secondNumber;
+    int subtractResult = firstNumber - secondNumber;
+    int divideResult = firstNumber / secondNumber;
+    int multiplyResult = firstNumber*secondNumber;}
+
+    public class Klakulators
+        public static void main(String[] args)
+
+        system.out.println("Sum result:" + sumResult);
+        system.out.println("Subtract result:" + subtractResult);
+        system.out.println("divide result:" + divideResult);
+        system.out.println("Multiply result:" + multiplyResult);
+    }
