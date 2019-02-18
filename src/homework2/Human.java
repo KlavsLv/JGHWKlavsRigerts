5626baec5af637e1f761d0e9ac30abb145469c19
KlavsLv <boisxlv16@gmail.com>
2019-02-18 19:58:17 +0200

    Cilveks

---
+++ src/homework2/Human.java
@@ -0,0 +1,18 @@
+package homework2;
+
+public class Human {
+
+
+    public String getAge;
+    String name;
+    public int age;
+
+    public int getAge(){ return this.age; }
+    public void setAge(int age){this.age = age;}
+
+
+    public String getName() {return this.name;}
+    public void setName(String name){this.name = name;}
+
+    }
+
